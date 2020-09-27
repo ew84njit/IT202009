db865ab24b9a453e1e00eb7db5a676b3ec1bbf6d
ewangso <skyromex@gmail.com>
2020-09-27 15:20:54 -0400

    made some changes

--- loginreg/sample_reg.php
+++ loginreg/sample_reg.php
@@ -9,22 +9,26 @@
   <input type="submit" name="register" value="Register"/>
 </form>
 <?php
-if(isset($_POST["register"])){
+if(isset($_POST["register"]))
+{
   $email = null;
   $password = null;
   $confirm = null;
-  if(isset($_POST["email"])){
+  if(isset($_POST["email"]))
+  {
     $email = $_POST["email"];
   }
-  if(isset($_POST["password"])){
+  if(isset($_POST["password"]))
+  {
     $password = $_POST["password"];
   }
-  if(isset($_POST["confirm"])){
+  if(isset($_POST["confirm"]))
+  {
     $confirm = $_POST["confirm"];
   }
   $isValid = true;
-  //check if passwords match on the server side
-  if($password == $confirm){
+  if($password == $confirm)
+  {
     echo "Passwords match <br>"; 
   }
   else{
@@ -34,29 +38,31 @@ if(isset($_POST["register"])){
   if(!isset($email) || !isset($password) || !isset($confirm)){
    $isValid = false; 
   }
-  //TODO other validation as desired, remember this is the last line of defense
-  if($isValid){
+
+  if($isValid)
+  {
     $hash = password_hash($password, PASSWORD_BCRYPT);
     require_once("db.php");
     $db = getDB();
-	if(isset($db)){
-		//here we'll use placeholders to let PDO map and sanitize our data
-		$stmt = $db->prepare("INSERT INTO Users(email, password) VALUES(:email, :password)");
-		//here's the data map for the parameter to data
-		$params = array(":email"=>$email, ":password"=>$hash);
-		$r = $stmt->execute($params);
-		//let's just see what's returned
-		echo "db returned: " . var_export($r, true);
-		$e = $stmt->errorInfo();
-		if($e[0] == "00000"){
-			echo "<br>Welcome! You successfully registered, please login.";
-		}
-		else{
-			echo "uh oh something went wrong: " . var_export($e, true);
-		}
-	}
+
+    if(isset($db)){
+      $stmt = $db->prepare("INSERT INTO Users(email, password) VALUES(:email, :password)");
+      $params = array(":email"=>$email, ":password"=>$hash);
+      $r = $stmt->execute($params);
+
+      echo "db returned: " . var_export($r, true);
+      $e = $stmt->errorInfo();
+      if($e[0] == "00000"){
+        echo "<br>Welcome! You successfully registered, please login.";
+      }
+      else{
+        echo "uh oh something went wrong: " . var_export($e, true);
+      }
+    }
   }
-  else{
+
+  else
+  {
    echo "There was a validation issue"; 
   }
 }
