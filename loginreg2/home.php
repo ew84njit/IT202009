502313d6a438af621a7cb817d67a21f2438cd33c
ewangso <skyromex@gmail.com>
2020-10-05 16:04:37 -0400

    logreg3

--- loginreg2/home.php
+++ loginreg2/home.php
@@ -8,7 +8,6 @@ session_start();
   <li><a href="logout.php">Logout</a></li>
 </ul>
 <?php
-//we use this to safely get the email to display
 $email = "";
 if(isset($_SESSION["user"]) && isset($_SESSION["user"]["email"])){
  $email = $_SESSION["user"]["email"]; 
